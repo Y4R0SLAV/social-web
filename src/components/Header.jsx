57353b750bb6d47c9d2e496afe8c9a78086198a9
Y4ROSLAV <jeros1337@gmail.com>
2021-09-26 14:46:11 +0300

    create template for profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://yt3.ggpht.com/a/AATXAJwlb4OFgfzmzyNmNIwWwcWvcb-K-Ifv7QQB2bVUrw=s900-c-k-c0xffffffff-no-rj-mo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://phonoteka.org/uploads/posts/2021-03/1616706027_48-p-kartinki-dlya-fona-saita-53.jpg" />
+      </div>// https://avatars.mds.yandex.net/i?id=f35f03d96b85e770f1e7eae0cc530275-5305831-images-thumbs&n=13&exp=1
+      <div>
+        ava + desc
+      </div>
+      <div>
+        my posts
+        <div>
+          new post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
