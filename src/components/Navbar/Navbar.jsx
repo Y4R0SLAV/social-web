09eb5c745674281acf46f8c97db257dcb6df355e
Y4ROSLAV <jeros1337@gmail.com>
2021-09-26 23:21:04 +0300

    changed <a href=""> to <NavLink to="">

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from "./Navbar.module.css";
+import Music from './../Music/Music';
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
         <div className={s.nav__item}>
-          <a href="/profile">Profile</a>
+          <NavLink to="/profile" activeClassName={s.active}>Profile </NavLink>
         </div>
         <div className={s.nav__item}>
-          <a href="/dialogs">Messages</a>
+          <NavLink to="/dialogs" activeClassName={s.active}>Messages </NavLink>
         </div>
         <div className={s.nav__item}>
-          <a href="news" >News</a>
+          <NavLink to="news" activeClassName={s.active} >News </NavLink>
         </div>
         <div className={s.nav__item}>
-          <a href="music">Music</a>
+          <NavLink to="music" activeClassName={s.active}>Music </NavLink>
         </div>
         <div className={s.nav__item}>
-          <a href="settings">Settings</a>
+          <NavLink to="settings" activeClassName={s.active}>Settings </NavLink>
         </div>
       </nav>
   );
